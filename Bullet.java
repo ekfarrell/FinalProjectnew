3707d3e28d7e0ca0f7c3799b3366c0479816bf09
Matthew C Kunzer <MCKunzer@student.naperville203.org>
2017-05-19 14:35:00 -0500

    here you go

--- Bullet.java
+++ Bullet.java
@@ -19,7 +19,21 @@ public class Bullet extends GameObject
     }
     
     public void render(Graphics g){
-        
+        g.setColor(Color.red);
+        g.fillOval(x,y,radius,radius);
+    
     }
 
+    public boolean hitReg(LinkedList<GameObject> objects){
+        int distance;
+        for(int object = 1;object <= objects.size(); object++){
+            if(objects.get(object).getId() == ID.Asteroid){
+                distance = (int)Math.sqrt(Math.pow((x-objects.get(object).getX()),2)+Math.pow(y-objects.get(object).getY(),2));
+                if(distance < objects.get(object).getRadius()){
+                  return true;  
+                }
+            }
+        }
+        return false; 
+    }
 }
